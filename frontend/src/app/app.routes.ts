b92fed8def6de415dc78fa3b95339fef65daa6d4
Govind <heyitsmegovind123@gmail.com>
2025-06-25 18:27:31 +0530

    file upload done

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Upload } from './upload/upload';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'upload',component:Upload},
+    {path:'**', redirectTo:'upload'}
+];
--- frontend/src/app/upload/upload.ts
+++ frontend/src/app/upload/upload.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
-  imports: [],
+  imports: [CommonModule,
+    FormsModule,
+  ],
   templateUrl: './upload.html',
   styleUrl: './upload.css'
 })
 export class Upload {
+  selectedFile : File | null=null;
+  uploadMessage : string='';
 
+  constructor(private http : HttpClient){}
+
+  onFileSelected(event: any){
+    this.selectedFile = event.target.files[0];
+  }
+
+  onSubmit(){
+    if(!this.selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('logfile', this.selectedFile);
+
+    this.http.post('http://localhost:3000/upload',formData).subscribe({
+      next: (res) =>{
+        this.uploadMessage=' File uploaded successfully';
+        console.log(res);
+      },
+      error: (err) => {
+        this.uploadMessage = 'Upload failed';
+        console.error(err);
+      }
+    });
+
+  }
 }
